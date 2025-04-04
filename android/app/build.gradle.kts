860ac38875774d5d4832e7be028823bb01609c17
Lee Chen Wei <leechenwei7@gmail.com>
2025-04-04 18:22:07 +0800

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,26 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+dependencies {
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+}
+
 android {
     namespace = "com.example.food_link"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,7 +36,7 @@ android {
         applicationId = "com.example.food_link"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:food_link/utils/theme/theme.dart';
-import 'features/main_screen.dart';
+import 'features/login_page.dart';
 
 class FoodLink extends StatelessWidget {
   const FoodLink({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
       theme: FLAppTheme.lightTheme,
       darkTheme: FLAppTheme.darkTheme,
-      home: const MainScreen(),
+      home: LoginPage(),
     );
   }
 }
